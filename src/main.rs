528c3211e913921b9706f5c8cf7f071cc7c76c25
Abhishek Shah <abhishekshah3@gmail.com>
2024-03-20 22:31:09 +0530

    Switch to Arc<Mutex> for upcoming eg. Demonstrate deadlock still holds

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use tokio::time::Duration;
 
 #[tokio::main]
 async fn main() {
-    let mtx = Mutex::new(0);
+    let mtx = Arc::new(Mutex::new(0));
     // both tasks are run concurrently on a single thread - this is key to the deadlock
-    tokio::join!(work(&mtx), work(&mtx));
+    tokio::join!(work(mtx.clone()), work(mtx.clone()));
 
+    // Solution 1 : Double the runtime threads
+    // tokio::spawn(async { work(&mtx).await });
     println!("{}", *mtx.lock().unwrap());
 }
 
-async fn work(mtx: &Mutex<i32>) {
-    println!("lock");
+async fn work(mtx: Arc<Mutex<i32>>) {
+    let id = std::thread::current().id();
+    println!("Task id {:?} started", id);
     {
         let mut _v = mtx.lock().unwrap();
-        println!("locked");
+        println!("Task id: {:?} locked mutex", id);
         // slow network request
         // Note the .await. A std::sync::Mutex lock is held across this .await
         // Let the two tasks be T1 and T2
@@ -24,5 +27,5 @@ async fn work(mtx: &Mutex<i32>) {
         // mtx unlock, and T1 cannot resume because T2 is taking up the entire thread
         tokio::time::sleep(Duration::from_millis(100)).await;
     }
-    println!("unlock")
+    println!("Task id: {:?} unlocked mutex", id);
 }
